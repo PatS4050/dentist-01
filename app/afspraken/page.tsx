395a89730b0369ffd7bd618bc6fd699b09eb5148
PatS4050 <patrick@straatman.eu>
2026-09-21 14:05:31 +0200

    routng afspraken 1 gemaakt

--- app/afspraken/page.tsx
+++ app/afspraken/page.tsx
@@ -1,15 +1,39 @@
 import Header from "@/components/Header";
+import Link from "next/link";
 
 export default function Afspraken() {
     return (
         <main className="page-container">
-            <div>
-                <Header icon="/logo.svg" title="Tandartspraktijk de Tandenborstel"/>
-                <section className="hero">
-                    <div className="hero-content">
-                        <h2>Afspraken</h2>
-                    </div>
-                </section>
-            </div>
+            <Header icon="/logo.svg" title="Afspraken"/>
+
+            <section className="intro">
+                <h2>Een afspraak maken</h2>
+                <p>
+                    Wilt u een controle plannen, heeft u een klacht of wilt u meer
+                    informatie over een behandeling? Neem dan contact met ons op. Bij ernstige pijn, een afgebroken tand
+                    of een ongeval kunt u het beste direct telefonisch contact met ons opnemen.
+                </p>
+            </section>
+
+            <section className="card-container">
+                <article className="card">
+                    <h3>Telefonisch</h3>
+                    <p>U kunt ons tijdens openingstijden telefonisch bereiken.</p>
+                    <strong>030 - 123 45 67</strong>
+                </article>
+
+                <article className="card">
+                    <h3>Openingstijden</h3>
+                    <p>Maandag t/m vrijdag</p>
+                    <p>08:00 - 17:00 uur</p>
+                </article>
+
+                <article className="card">
+                    <h3>Online afspraken</h3>
+                    <p>Wilt u een controle plannen of heeft u een klacht? U kunt eenvoudig een afspraak maken.</p>
+                    {/*<a href="#" className="link-button"> Maak een afspraak </a>*/}
+                    <Link href="/afspraken/maken" className="link-button">Maak een afspraak</Link>
+                </article>
+            </section>
         </main>
     )};
