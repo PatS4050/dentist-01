bc6c74b42507354a4f5f3ef505dc5fd6b3c84669
PatS4050 <patrick@straatman.eu>
2026-09-21 13:41:28 +0200

    paginas aangemaakt

---
+++ app/bleken/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+
+export default function Bleken() {
+    return (
+        <main className="page-container">
+            <div>
+                <Header icon="/logo.svg" title="Tandartspraktijk de Tandenborstel"/>
+                <section className="hero">
+                    <div className="hero-content">
+                        <h2>Bleken</h2>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )};
---
+++ app/gaatjes/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/Header";
+
+export default function Gaatjes() {
+    return (
+        <main className="page-container">
+            <div>
+                <Header icon="/logo.svg" title="Tandartspraktijk de Tandenborstel"/>
+                <section className="hero">
+                    <div className="hero-content">
+                        <h2>Gaatjes</h2>
+                    </div>
+                </section>
+            </div>
+        </main>
+    )};
